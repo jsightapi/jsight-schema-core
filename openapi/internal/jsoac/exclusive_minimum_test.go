236bc32421a58827dcdef058228aa80dda7dacbc
chepil <den@chepil.ru>
2024-02-19 12:42:45 +0300

    SERV-282 fix

--- openapi/internal/jsoac/exclusive_minimum_test.go
+++ openapi/internal/jsoac/exclusive_minimum_test.go
@@ -36,6 +36,30 @@ func Test_newOpenAPIExclusiveMinimum(t *testing.T) {
 				"exclusiveMinimum": true
 			}`,
 		},
+		{
+			`1 // { min: 0, exclusiveMinimum: false }`,
+			`{
+				"type": "integer", 
+				"example": 1,
+				"minimum": 0
+			}`,
+		},
+		{
+			`1.12 // { min: 0, exclusiveMinimum: false }`,
+			`{
+				"type": "number", 
+				"example": 1.12,
+				"minimum": 0
+			}`,
+		},
+		{
+			`0.12 // { min: 0, exclusiveMinimum: false }`,
+			`{
+				"type": "number", 
+				"example": 0.12,
+				"minimum": 0
+			}`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.jsight, func(t *testing.T) {
