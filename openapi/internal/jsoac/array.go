9c8306e543e348375e6f790e76ce5b78b89d5488
chepil <den@chepil.ru>
2024-02-26 17:48:46 +0300

    SERV-255 fix

--- openapi/internal/jsoac/array.go
+++ openapi/internal/jsoac/array.go
@@ -4,23 +4,38 @@ import (
 	schema "github.com/jsightapi/jsight-schema-core"
 )
 
+type AnyOf struct {
+	Items []Node `json:"anyOf"`
+}
+
 type Array struct {
-	jstType schema.TokenType
-	OADType OADType `json:"type"`
-	Items   []Node  `json:"items"`
+	jstType  schema.TokenType
+	OADType  OADType    `json:"type"`
+	Items    ArrayItems `json:"items"`
+	Nullable *Nullable  `json:"nullable,omitempty"`
+	MaxItems *int64     `json:"maxItems,omitempty"`
 }
 
 func newArray(astNode schema.ASTNode) Array {
+	maxItems := int64RefByString("")
+	if len(astNode.Children) == 0 {
+		maxItems = int64Ref(0)
+	}
 	a := Array{
-		OADType: OADTypeArray,
-		Items:   make([]Node, 0, len(astNode.Children)),
+		OADType:  OADTypeArray,
+		Items:    newArrayItems(len(astNode.Children)),
+		Nullable: newNullable(astNode),
+		MaxItems: maxItems,
 	}
-
 	for _, an := range astNode.Children {
-		a.Items = append(a.Items, newNode(an))
+		a.appendItem(an)
 	}
-
 	return a
+}
+
+func (a *Array) appendItem(astNode schema.ASTNode) {
+	value := newNode(astNode)
+	a.Items.append(value)
 }
 
 func (a Array) JSightTokenType() schema.TokenType {
