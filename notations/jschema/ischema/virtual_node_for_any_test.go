4f03a3047b3fb6da6e08615df7900c3fe2528b13
Andrei Ivanov <9808017@gmail.com>
2024-04-02 13:25:54 +0300

    SERV-367 Add method for creating ischema.Node impl for type any

--- notations/jschema/ischema/virtual_node_for_any_test.go
+++ notations/jschema/ischema/virtual_node_for_any_test.go
@@ -1,7 +1,9 @@
-package jschema
+package ischema
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/assert"
 
@@ -11,7 +13,9 @@ import (
 func Test_VirtualNodeForAny_Data(t *testing.T) {
 	n := VirtualNodeForAny()
 
-	assert.NotNil(t, n.Constraint(constraint.AnyConstraintType))
+	c := n.Constraint(constraint.TypeConstraintType)
+	require.NotNil(t, c)
+	require.Equal(t, `"any"`, c.(*constraint.TypeConstraint).Bytes().String())
 }
 
 func Test_VirtualNodeForAny_Same(t *testing.T) {
